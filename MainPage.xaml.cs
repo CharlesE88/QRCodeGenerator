379e46dc3b6e5e728832bc32a2ea3b9c7c218642
Charles E <Charles.doyleeudy@gmail.com>
2022-07-02 17:07:26 -0400

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using ZXing;
+using ZXing.Common;
+using ZXing.QrCode;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace InventoryScanner
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        static void Main(string[] args)
+        {
+            // create a barcode reader instance
+            var barcodeReader = new BarcodeReader();
+
+            // create an in memory bitmap
+            var barcodeBitmap = System.Drawing.Imaging.PixelFormat.Format32bppRgb;
+
+            // decode the barcode from the in memory bitmap
+            var barcodeResult = barcodeReader.Decode(barcodeBitmap);
+
+            // output results to console
+            Console.WriteLine($"Decoded barcode text: {barcodeResult?.Text}");
+            Console.WriteLine($"Barcode format: {barcodeResult?.BarcodeFormat}");
+        }
+    }
+}
